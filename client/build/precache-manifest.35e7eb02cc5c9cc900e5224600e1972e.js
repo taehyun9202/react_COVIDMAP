0ed3d1723729d62d053729d51fd2334266a3d143
Tyler <taehyun9202@hotmail.com>
2020-08-24 19:22:31 -0400

    second commit

---
+++ client/build/precache-manifest.35e7eb02cc5c9cc900e5224600e1972e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3a7beaedaeda83bb0499edc265160c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47576a1043863fdf7155",
+    "url": "/static/css/main.2bb0b8c7.chunk.css"
+  },
+  {
+    "revision": "779d857636e6283f1294",
+    "url": "/static/js/2.94a6efea.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/static/js/2.94a6efea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47576a1043863fdf7155",
+    "url": "/static/js/main.30219a36.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
